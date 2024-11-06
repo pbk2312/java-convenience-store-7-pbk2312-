693bcb2a20fe73605203b65820791891d51e5882
Park Yu Han <pbk2312@naver.com>
2024-11-06 23:41:17 +0900

    test(validator): 재고가 없는 상품 선택 시 예외 발생 테스트 케이스 추가
    - 재고가 0인 '[우아한돼지들-1]' 선택 시 INVALID_QUANTITY 예외가 발생하는 코드 추가

--- src/test/java/store/validator/InputValidatorTest.java
+++ src/test/java/store/validator/InputValidatorTest.java
@@ -18,25 +18,28 @@ class InputValidatorTest {
     @CsvSource({
             "'', EMPTY_INPUT",                           // 빈 입력
             "'[-10]', INVALID_FORMAT",                   // 상품명이 빈 경우
-            "'[콜라-0]', ZERO_QUANTITY",                 // 수량이 0인 경우
+            "'[콜라-0]', ZERO_QUANTITY",                 // 요청한 상품의 수량이 0인 경우
             "'[콜라=10]', INVALID_FORMAT",               // 형식 오류 - 잘못된 구분자
             "'[콜라-10][사이다-3]', INVALID_FORMAT",     // 형식 오류 - 쉼표 없이 구분
             "'[콜라10],[사이다-3]', INVALID_FORMAT",     // 형식 오류 - 하이픈 없는 경우
             "'[콜라-10],[사이다-0]', ZERO_QUANTITY",     // 여러 상품 중 하나의 수량이 0인 경우
             "'[없는상품-3]', NON_EXISTENT_PRODUCT",      // 존재하지 않는 상품
             "'[콜라-15]', INVALID_QUANTITY",             // 재고보다 많은 수량
-            "'[콜라--15]', INVALID_FORMAT"               // 잘못된 구분자
+            "'[콜라--15]', INVALID_FORMAT",              // 잘못된 구분자
+            "'[우아한돼지들-1]', INVALID_QUANTITY"         // 재고가 없는 상품 선택 시 예외 발생
     })
     void shouldThrowExceptionForInvalidProductSelection(String input, String expectedErrorCode) {
         // 샘플 상품 재고 구성
         Map<String, Integer> productInventory = new HashMap<>();
         productInventory.put("콜라", 10);
+        productInventory.put("우아한돼지들", 0);  // 우아한돼지들은 재고가 없는 상태로 설정
         productInventory.put("사이다", 5);
 
         assertThatThrownBy(() -> InputValidator.validateProductSelection(input, productInventory))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.valueOf(expectedErrorCode).getMessage());
     }
+
 
     @Test
     @DisplayName("정상 입력 값에 대해 예외가 발생하지 X")
