df437d2b84a22224e99af558458676feaa884d54
Park Yu Han <pbk2312@naver.com>
2024-11-06 14:13:17 +0900

    chore: InputViewTest 폴더 위치 변경
    - 테스트 코드 구조 정리를 위해 InputViewTest 위치를 store/view 이동

--- src/test/java/store/view/InputViewTest.java
+++ src/test/java/store/view/InputViewTest.java
@@ -1,4 +1,4 @@
-package store;
+package store.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import store.view.InputView;
 
 class InputViewTest {
 
