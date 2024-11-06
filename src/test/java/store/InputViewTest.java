2b99cf179966d8c328653af778d38ed4b426d4c3
Park Yu Han <pbk2312@naver.com>
2024-11-06 13:59:38 +0900

    test(view): InputView 구현 후 테스트
    - 작성한 InputView -> 테스트

--- src/test/java/store/InputViewTest.java
+++ src/test/java/store/InputViewTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import store.view.InputView;
 
 class InputViewTest {
 
