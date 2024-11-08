5cb65b08e1fc12ab77658eb82ad115f6c429fa74
Park Yu Han <pbk2312@naver.com>
2024-11-08 12:50:08 +0900

    refactor(model): OnePlusOnePromotion에 getFreeQuantity 메서드 추가
    - 구매한 수량의 절반이 무료 증정되도록 getFreeQuantity 메서드 구현
    - 기존 calculateDiscountedPrice 메서드와 함께 1+1 프로모션의 할인 및 증정 로직 완성

--- src/main/java/store/model/OnePlusOnePromotion.java
+++ src/main/java/store/model/OnePlusOnePromotion.java
@@ -1,16 +1,15 @@
 package store.model;
 
 public class OnePlusOnePromotion implements PromotionStrategy {
-
     @Override
     public double calculateDiscountedPrice(int quantity, double price) {
         int payableQuantity = (quantity + 1) / 2;
         return payableQuantity * price;
     }
+    
 
     @Override
-    public String getType() {
-        return "1+1";
+    public int getFreeQuantity(int quantity) {
+        return quantity / 2;  // 구매한 수량의 절반이 무료 증정
     }
-
 }
