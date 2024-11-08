ca82298481a1a55cbde407688c648cd1a224c62f
Park Yu Han <pbk2312@naver.com>
2024-11-08 12:50:46 +0900

    refactor(model): TwoPlusOnePromotion에 getFreeQuantity 메서드 추가 및 할인 로직 개선
    - getFreeQuantity 메서드 추가하여 구매한 수량의 3분의 1이 무료 증정되도록 구현
    - calculateDiscountedPrice 메서드의 로직을 개선하여 수량의 3분의 2만 계산

--- src/main/java/store/model/TwoPlusOnePromotion.java
+++ src/main/java/store/model/TwoPlusOnePromotion.java
@@ -1,16 +1,16 @@
 package store.model;
 
 public class TwoPlusOnePromotion implements PromotionStrategy {
-
     @Override
     public double calculateDiscountedPrice(int quantity, double price) {
-        int payableQuantity = (quantity / 3) * 2 + (quantity % 3);
+        int payableQuantity = (2 * quantity) / 3;
         return payableQuantity * price;
     }
 
+
     @Override
-    public String getType() {
-        return "2+1";
+    public int getFreeQuantity(int quantity) {
+        return quantity / 3;  // 구매한 수량의 3분의 1이 무료 증정
     }
 
 }
